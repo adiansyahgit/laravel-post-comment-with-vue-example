34eba2e4ecceaf2f3554688c2aac16115e2a0853
Adiansyah <mail.adiansyah@gmail.com>
2023-07-21 21:28:26 +0700

    achieve: login using auth sanctum

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        if (!auth()->attempt($request->only(['email', 'password']))) {
+            throw ValidationException::withMessages([
+                'email' => ['Wrong credential']
+            ]);
+        }
+    }
+}
